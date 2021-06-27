f8e4f47087f862f723a72a647796d3b1cd435507
Чугреев Павел Владимирович <pvchugreev@domclick.ru>
2021-06-27 16:33:06 +0300

    изменение суммы корзины

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ const initialState = {
     menu: [],
     cartItems: [],
     loading: false,
-    error: false
+    error: false, 
+    total: 0
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -16,17 +17,16 @@ const reducer = (state = initialState, action) => {
         case 'ADD_TO_CART':
             
             const index = state.cartItems.findIndex(item => item.id == action.payload);
-             console.log(index)
 
             if(index >= 0){
                 const itemInState = state.menu.find(el => el.id == action.payload);
-                console.log(itemInState)
                 const newItem = {
                     ...itemInState, 
                     count: ++itemInState.count
                 }
                 return {
                     ...state,
+                    total: state.total + newItem.price,
                     cartItems: [
                         ...state.cartItems.slice(0, index),
                         newItem,
@@ -38,18 +38,21 @@ const reducer = (state = initialState, action) => {
                 item.count = 1
                 return {
                     ...state,
+                    total: state.total + item.price,
                     cartItems: [...state.cartItems, item] 
                 }
             }
             
 
         case 'DELETE_FROM_CART':
-            const id = action.payload
-            const idx = state.cartItems.findIndex(item => item.id == id);
+            const idx = state.cartItems.findIndex(item => item.id == action.payload);
 
             if(idx >= 0){
+                const item = state.cartItems.find(el => el.id == action.payload);
+
                 return {
                     ...state,
+                    total: state.total-item.count*item.price,
                     cartItems: [
                         ...state.cartItems.slice(0, idx), 
                         ...state.cartItems.slice(idx + 1)
